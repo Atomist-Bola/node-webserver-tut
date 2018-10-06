0869b368446bf2338170afdff0e072daab78e96b
Bolanle Bincom <bolanle.bincom@gmail.com>
2018-10-06 15:44:24 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+console.log('Starting server.js');
+
+var fs = require('fs');
+var express = require('express');
+var hbs = require('hbs');
+hbs.registerPartials('/Users/bincom/Desktop/project/Bolanle/node/nodeServers/views/partials'); 
+hbs.registerHelper('getYear', ()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('getMonth', ()=>{
+    return new Date().getMonth();
+})
+var app = express();
+
+// Express Middlewares
+app.use(express.static(__dirname + '/public') );
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs');
+// })
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if (err){
+            console.log('There was a problem here');
+            
+        }
+    });
+    console.log(log); 
+    next();
+});
+
+
+
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        name: 'Bolanle',
+        activity: 'Practicing', 
+        likes: ['eating',
+         'coding', 
+          'sleeping',
+           'gaming',
+        {
+            word: 'Work',
+        }]
+    })
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs', {
+        title: 'About Page'
+       
+    });
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+})
